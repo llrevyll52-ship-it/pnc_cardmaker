5478c276f6e35af39e2c3aa962a50d4633647d58
rallx_x <llrevyll52@gmail.com>
2026-06-05 13:49:57 +0900

    Add basic input handlers

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+console.log("PNC Card Maker Loaded");
 
+const cardName = document.getElementById("cardName");
+const cardTitle = document.getElementById("cardTitle");
+
+cardName.addEventListener("input", () => {
+    console.log("이름:", cardName.value);
+});
+
+cardTitle.addEventListener("input", () => {
+    console.log("타이틀:", cardTitle.value);
+});
